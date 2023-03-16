4500b2da7a791a9aea2829d6d199a44a807b36e5
vishal <vishalnitavne29@gmail.com>
2023-03-16 11:07:42 +0530

    first test case created

--- test2.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient("mongodb://localhost:27017")
-print("Connection Successful")
-db = client["example"]
-col = db["temp"]
-x = col.find()
-for i in x:
-    print(i)
-
